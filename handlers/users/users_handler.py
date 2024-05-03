a1e7572f9795dc99af90eb421b39760627559bf6
anon <anon>
2024-05-04 00:46:08 +0300

    registration and login

--- handlers/users/users_handler.py
+++ handlers/users/users_handler.py
@@ -1,17 +1,38 @@
-from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.builtin import Command
 from aiogram.types import Message
 
+from data.config import PHONES_TABLE_NAME, USERS_TABLE_NAME
 from data.db import database_connection
 from loader import dp
+from models.users_handler_models import User
 from states.UserStates import UserStates
 from utils.users_handler_utils import is_phone_number_valid, PhoneData, build_phone_data_line
 
 
+def loginOrRegister(telegram_id: int, alias: str, username: str) -> str:
+    search_list = database_connection.execute(
+        f"SELECT * FROM {USERS_TABLE_NAME} WHERE \"telegram_id\" = ?", [telegram_id]
+    ).fetchall()
+
+    answer: str
+
+    if len(search_list) > 0:
+        existed_user = User(**search_list[0])
+        answer = f"Раді бачити Вас знову, {existed_user.alias}!"
+    else:
+        database_connection.execute(
+            f"INSERT INTO \"{USERS_TABLE_NAME}\" (telegram_id, alias, username) values (?,?,?)", [telegram_id, alias, username]
+        )
+        answer = f"Доброго дня, {alias}, Вас було успішно зареєстровано!"
+
+    return answer
+
+
 @dp.message_handler(Command("start"))
 async def bot_start(message: Message):
-    await message.answer(f" {message.from_user.full_name}, доброго дня!")
+    login_report = loginOrRegister(message.from_user.id, message.from_user.full_name, message.from_user.username)
+    await message.answer(login_report)
 
 
 @dp.message_handler(Command("search"))
@@ -26,7 +47,8 @@ async def search_by_number(message: Message, state: FSMContext):
     number = number.replace(" ", "")
     if is_phone_number_valid(number):
         results: list[dict] = (
-            database_connection.execute("SELECT * FROM phones WHERE \"phone_number\" = ?", [number]).fetchall())
+            database_connection.execute(f"SELECT * FROM {PHONES_TABLE_NAME} WHERE \"phone_number\" = ?",
+                                        [number]).fetchone())
         report: str
         if len(results) > 0:
             report = "\n".join(map(lambda result: build_phone_data_line(PhoneData(**result)), results))
--- models/users_handler_models.py
+++ models/users_handler_models.py
@@ -7,3 +7,11 @@ class PhoneData:
         self.__dict__.update(kwargs)
 
 
+class User:
+    id: int
+    username: str
+    alias: str
+    telegram_id: str
+
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
