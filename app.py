ea18a98297187fbd4a25ae7357d504ab252ea558
anon <anon>
2024-05-03 22:54:08 +0300

    gitignore update

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ async def on_startup(dp):
     await set_default_commands(dp)
 
 
-
 if __name__ == '__main__':
     from aiogram import executor
     from handlers import dp
+
     executor.start_polling(dp, on_startup=on_startup)
--- handlers/users/start.py
+++ handlers/users/start.py
@@ -6,8 +6,8 @@ from loader import dp
 @dp.message_handler(Command("start"))
 async def bot_start(message: types.Message):
     await message.answer(f" {message.from_user.full_name}, здравствуйте.")
+
+
 @dp.message_handler(Command("search"))
 async def bot_start(message: types.Message):
     await message.answer(f" {message.from_user.full_name}, введите номер телефона, какой хотите проверить.")
-
-
